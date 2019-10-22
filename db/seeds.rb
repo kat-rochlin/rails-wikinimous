63699b3da5f4d951909c97814bd7a819e063fbbb
Katrina Rochlin <kat.rochlin@gmail.com>
2019-10-22 17:55:13 +0800

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require "faker"
+
+
+10.times do
+  article = Article.new(
+    title: Faker::Movies::HarryPotter.character,
+    content: Faker::Movies::HarryPotter.quote
+  )
+  puts "article #{article.title} created"
+  article.save
+end
